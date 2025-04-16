b52d024399799dd484b4b65d55f4956d9abf42c7
prajwallshetty <prajwal0shetty11@gmail.com>
2025-04-16 10:41:10 +0530

    added new features

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -41,19 +41,21 @@
     }
 
     // Slider Range JS
-    if ( $("#slider-range").length ) {
+    if ($("#slider-range").length) {
         $("#slider-range").slider({
             range: true,
             min: 0,
             max: 5000,
             values: [0, 5000],
             slide: function (event, ui) {
-                $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+                $("#amount").val("₹" + ui.values[0] + " - ₹" + ui.values[1]);
             }
         });
-        $("#amount").val("$" + $("#slider-range").slider("values", 0) +
-            " - $" + $("#slider-range").slider("values", 1));
+    
+        $("#amount").val("₹" + $("#slider-range").slider("values", 0) +
+            " - ₹" + $("#slider-range").slider("values", 1));
     }
+    
 
     /*------ Hero slider 1 ----*/
     $('.hero-slider-1').slick({
--- resources/views/livewire/sidebar.blade.php
+++ resources/views/livewire/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="col-lg-3 primary-sidebar sticky-sidebar">
+<div class="col-lg-3 primary-sidebar sticky-sidebar mt-75">
     <div class="row">
         <div class="col-lg-12 col-mg-6">
             @if (session()->has('error'))
@@ -26,25 +26,5 @@
             @endforeach
 
         </ul>
-    </div>
-    <!-- Fillter By Price -->
-    <div class="sidebar-widget price_range range mb-30 rounded-3xl border-slate-200 shadow-md" style="border-radius: 30px">
-        <h1 class="font-bold text-lg tracking-widest text-orange-500 mb-4 uppercase">Filter by price</h1>
-        <div class="price-filter">
-            <form action="{{ url()->current() }}">
-                <div class="price-filter-inner">
-                    <div id="slider-range"></div>
-                    <div class="price_slider_amount">
-                        <div class="label-input">
-                            <span>Range:</span>
-                            <input type="text" id="amount" name="range" placeholder="Required format ₹xx - ₹xx">
-                        </div>
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-sm">
-                    Apply Filter
-                </button>
-            </form>
-        </div>
     </div>
 </div>
