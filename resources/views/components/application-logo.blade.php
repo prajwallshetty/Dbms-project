0245a58aeac5441006fe5b09688ace17c377ed7c
rakshithashettyyy <rakshashetty4545@gmail.com>
2025-04-20 23:15:17 +0530

    changed the logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
 <div class="logo w-[12rem]">
-    <a href="{{ route('home') }}"><img src="{{ asset('assets/imgs/logo/ll.png')}}" class="" alt="logo"></a>
+    <a href="{{ route('home') }}"><img src="{{ asset('assets/imgs/logo/LL.png')}}" class="" alt="logo"></a>
 </div>
